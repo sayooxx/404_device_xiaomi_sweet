d3bec4f46b0299ac019f06507d44af26c566ee03
Mikhail Naganov <mnaganov@google.com>
2021-02-17 20:36:38 +0000

    sweet: Switch audio HAL to V7
    Updated audio policy manager configuration files to comply
    with the changes in the XML schema. This was done automatically
    by running the following commands:
    
      hardware/interfaces/audio/7.0/config/update_audio_policy_config.sh \
          device/xaomi/sweet/configs/audio/audio/audio_policy_configuration.xml 6.0
    
    Bug: 142480271
    Test: atest VtsHalAudioV7_0TargetTest
    Test: atest VtsHalAudioEffectV7_0TargetTest
    Change-Id: I55d3728c809d27e58d5d496a1c7d637050dde53a

--- device.mk
+++ device.mk
@@ -27,8 +27,8 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
-    android.hardware.audio.effect@6.0-impl \
-    android.hardware.audio@6.0-impl
+    android.hardware.audio.effect@7.0-impl \
+    android.hardware.audio@7.0-impl
 
 PRODUCT_PACKAGES += \
     audio.r_submix.default \
@@ -43,7 +43,7 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration_7_0.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
