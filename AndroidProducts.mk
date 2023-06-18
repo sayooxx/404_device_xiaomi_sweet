c71d3f3d3d4fce86a0fad82225383ad595c2a315
sayooxx <metech87@gmail.com>
2023-06-18 19:07:42 +0530

    sweet: Welcome p404

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2023 Project 404
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet.mk
+    $(LOCAL_DIR)/p404_sweet.mk
+
+COMMON_LUNCH_CHOICES := \
+    p404_sweet-user \
+    p404_sweet-userdebug \
+    p404_sweet-eng
--- p404_sweet.mk
+++ p404_sweet.mk
@@ -11,10 +11,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Project 404 stuff
+$(call inherit-product, vendor/404/config/common.mk)
+WITH_GAPPS := true
 
-PRODUCT_NAME := lineage_sweet
+
+PRODUCT_NAME := p404_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
