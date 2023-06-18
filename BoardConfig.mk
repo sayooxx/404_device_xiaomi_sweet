4251f8d7aaaebd8ceb454ee09fba0d95f45c4331
sayooxx <metech87@gmail.com>
2023-06-18 18:25:54 +0530

    sweet: Remove LineageOS FCM path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,7 +77,6 @@ KERNEL_LD := LD=ld.lld
 # Manifest
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/manifests/device_framework_matrix.xml \
-    $(DEVICE_PATH)/manifests/lineage_device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifests/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/manifests/compatibility_matrix.xml
 ODM_MANIFEST_SKUS := nfc
